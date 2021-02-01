a003a5abaaf9dd8f5f4a3cc78e5693770ee45665
Michael Flynn <flynn.michael.e@gmail.com>
2021-02-01 11:28:31 -0600

    Initial commit

---
+++ R/get_troopdata.R
@@ -0,0 +1,52 @@
+#' Load Troop Data
+#'
+#'
+#' @param host The Correlates of War (COW) numeric country code for the host country or countries in the series
+#' @param startyear the first year for the series
+#' @param endyear The last year for the series
+#' @return Returns a data frame containing country  year observations for troop deployments
+#' @export
+
+get_troopdata <- function(host = NA, branch = FALSE, startyear, endyear) {
+
+  tempdata <- troopdata::troopdata
+
+  # Set warning for year range
+  if(startyear < 1950 | endyear > max(tempdata$year)) {
+    stop("Specified year is out of range. Available range includes 1950 through 2020.")
+  }
+
+  if(branch) {
+    warn("Branch data only available for 2006 forward.")
+  }
+
+  if (is.na(host)) {
+
+    tempdata <- tempdata %>%
+      dplyr::filter(year >= startyear & year <= endyear)
+
+  }
+
+  else {
+
+    host <- c(host)
+
+    tempdata <- tempdata %>%
+      dplyr::filter(ccode %in% host & year >= startyear & year <= endyear)
+
+  }
+
+  if (branch) {
+
+    tempdata <- tempdata %>%
+      dplyr::select(ccode, year, troops, army, navy, air_force, marine_corps)
+
+  } else {
+
+    tempdata <- tempdata %>%
+      dplyr::select(ccode, year, troops)
+
+  }
+
+}
+
