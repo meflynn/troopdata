0c1ad77fd2a25da920df128c9163ceb7c8c67f95
Michael Flynn <flynn.michael.e@gmail.com>
2021-02-02 11:08:42 -0600

    Update with troops data correcting Ethiopia error.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,6 @@
 ## code to prepare `DATASET` dataset goes here
 
-troopdata <- readstata13::read.dta13(here::here("~Dropbox/Projects/Minerva grant documents/Book/Raw Data/troops 1950-2020.dta")) %>%
+troopdata <- haven::read_dta(here::here("../../Projects/Troops/troops 1950-2020.dta")) %>%
   dplyr::select(ccode, year, troops, army, navy, air_force, marine_corps)
 
 usethis::use_data(troopdata, overwrite = TRUE)
